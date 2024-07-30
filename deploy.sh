3adf344032d8b9f65b199844b72e1571f0671dc2
joonhan <joonhan@student.42seoul.kr>
2024-07-30 15:11:47 +0900

    chore: 배포 스크립트 생성

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+APPLICATION_NAME=backend
+
+IS_APPLICATION_RUNNING=$(sudo docker compose ls | grep $APPLICATION_NAME | grep running | sed 's/.*/true/')
+
+if [ $IS_APPLICATION_RUNNING = "true" ]; then
+    echo "Application is running. Starting the application..."
+
+    local is_blue_running=$(sudo docker compose -p $APPLICATION ps | grep blue | grep Up | sed 's/.*/true/')
+    if [ $is_blue_running = "true" ]; then
+        echo "Blue is running. Turning on the green..."
+        cp 
+        sudo docker compose -p $APPLICATION-green up -d
+
+        # green healthcheck
+
+        echo "Green is running. Stopping the blue..."
+        sudo docker compose -p $APPLICATION-blue down
+    else
+        echo "Green is running. Stopping the green..."
+        sudo docker compose -p $APPLICATION-green down
+    fi
+    sudo docker compose up -d
+else
+    echo "Application is not running. Starting the application..."
+    sudo docker compose up -d
+fi
